3810e011c4b96b032ee68be5671f5356af811562
Sarath-Nivilesh <nivilesh1234@gmail.com>
2025-07-01 20:19:10 +0530

    assesment lumel

---
+++ src/components/App.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import RowComponent from "./RowComponent";
+import initialData from "../utils/InitialData.json";
+
+export default function App() {
+  const [data, setData] = useState(initialData);
+
+ 
+  const updateValue = (id, newValue, type) => {
+    const newData = structuredClone(data);
+    const updateTree = (node) => {
+      if (node.id === id) {
+        if (type === "value") {
+          node.value = newValue;
+        } else if (type === "distribute") {
+          distributeValue(node, newValue);
+        }
+      }
+      if (node.children) {
+        node.children.forEach(updateTree);
+        node.value = node.children.reduce((sum, c) => sum + c.value, 0);
+      }
+    };
+
+    newData.rows.forEach(updateTree);
+    setData(newData);
+  };
+
+const distributeValue = (node, newValue) => {
+  const leaves = [];
+  const getLeaves = (n) => {
+    if (!n.children) leaves.push(n);
+    else n.children.forEach(getLeaves);
+  };
+  getLeaves(node);
+
+  const originalTotal = leaves.reduce((sum, l) => sum + l.value, 0);
+
+  leaves.forEach((leaf) => {
+    const weight = leaf.value / originalTotal;
+    leaf.value = Math.round(newValue * weight * 100) / 100; 
+  });
+};
+
+  const FinalTotal = data.rows.reduce((sum, elem) => sum + elem.value, 0);
+
+  return (
+    <div>
+      <h2>Sarath-Hierarchical Table Assessement</h2>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Label</th>
+            <th>Value</th>
+            <th>Input</th>
+            <th>Allocation %</th>
+            <th>Allocation Val</th>
+            <th>Variance %</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.rows.map((row) => (
+            <RowComponent
+              key={row.id}
+              node={row}
+              originalNode={initialData.rows.find((r) => r.id === row.id)}
+              updateValue={updateValue}
+              level={0}
+            />
+          ))}
+          <tr>
+            <td>Final Total</td>
+            <td >{FinalTotal}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
---
+++ src/components/RowComponent.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+
+export default function RowComponent({
+  node,
+  originalNode,
+  updateValue,
+  level
+}) {
+  const [input, setInput] = useState("");
+
+  
+
+
+  const handleApplyPercentage = () => {
+    // console.log('handleApplyPercentage',input)
+  const percent = parseFloat(input);
+  if (isNaN(percent)) return;
+
+  const increaseAmount = (node.value * percent) / 100;
+  const newValue = node.value + increaseAmount;
+
+  if (node.children) {
+    updateValue(node.id, newValue, "distribute"); 
+  } else {
+    updateValue(node.id, newValue, "value");
+  }
+
+  setInput("");
+};
+
+
+  const handleApplyVal = () => {
+    const val = parseFloat(input);
+    if (!isNaN(val)) {
+      if (node.children) {
+        updateValue(node.id, val, "distribute");
+      } else {
+        updateValue(node.id, val, "value");
+      }
+      setInput("");
+    }
+  };
+
+  const getOriginalValue = () => {
+    if (!originalNode) return null;
+    if (originalNode.children) {
+      return originalNode.children.reduce((s, c) => s + c.value, 0);
+    }
+    return originalNode.value;
+  };
+
+const variance =
+  ((node.value - getOriginalValue()) / getOriginalValue()) * 100;
+
+  return (
+    <>
+      <tr>
+        <td style={{ paddingLeft: `${level * 5}px` }}>{level >0 ? `--` : false}{node.label}</td>
+        <td>{node.value}</td>
+        <td>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="number"
+          />
+        </td>
+        <td>
+          <button onClick={handleApplyPercentage}>Apply %</button>
+        </td>
+        <td>
+          <button onClick={handleApplyVal}>Apply Val</button>
+        </td>
+        <td>{`${variance.toFixed(2)}%`}</td>
+      </tr>
+      {node.children &&
+        node.children.map((child) => (
+          <RowComponent
+            key={child.id}
+            node={child}
+            originalNode={
+              originalNode?.children?.find((c) => c.id === child.id)
+            }
+            updateValue={updateValue}
+            level={level + 1}
+          />
+        ))}
+    </>
+  );
+}
